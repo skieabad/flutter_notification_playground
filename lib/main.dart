cf12abbf86f8db442e96ac326c79befbc26b6ab2
Wilson Abad <abadskie19@gmail.com>
2024-12-28 02:17:37 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notification_playground/firebase_options.dart';
 import 'package:flutter_notification_playground/homepage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
