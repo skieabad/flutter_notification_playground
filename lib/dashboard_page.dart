31cb93d3c6cd49d9e76d1012a7eb46340e425773
Wilson Abad <abadskie19@gmail.com>
2024-12-30 04:04:26 +0800

    add dashboard page

---
+++ lib/dashboard_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text('Welcome to the dashboard!'),
+        ),
+      ),
+    );
+  }
+}
